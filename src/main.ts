71cbd800f2c1c5696cd6c14c8cf54984335d7f7c
MattiaTaglia <mattia.tagliaferri9@gmail.com>
2024-07-02 16:13:24 +0200

    CBAM-28

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,58 @@
 export const add = (a: number, b: number) => a + b;
 export const sub = (a: number, b: number) => a - b;
+
+import soap from 'soap';
+
+export async function asyncRequestListDeclarationDocuments(params: {
+  data: {
+    xml: string;
+    dichiarante: string;
+  },
+  security: {
+    certificate: Blob | string;
+    passphrase: string;
+  }
+}): Promise<{
+  type: string;
+  message: unknown;
+}> {
+  
+  const url = './assets/ponimport_reale.wsdl';
+  const xmlParams = {
+    serviceId: 'richiestaListaDocumentiDichiarazione',
+    data: params.data
+  }
+
+  try {
+
+    let certificate: Buffer | string | undefined = undefined
+
+    if (params.security.certificate instanceof Blob) {
+      const arrayBuffer = await params.security.certificate.arrayBuffer();
+      certificate = Buffer.from(arrayBuffer);
+    } else {
+      certificate = params.security.certificate;
+    }
+
+    const client = await soap.createClientAsync(url);
+    client.setSecurity(
+      new soap.ClientSSLSecurityPFX(
+        certificate,
+        params.security.passphrase
+      )
+    );
+
+    const resultProcess = await client.processAsync(xmlParams);
+
+    return {
+      'type': 'success',
+      'message': resultProcess
+    };
+  } catch (err) {
+    return {
+      'type': 'error',
+      'message': err
+    };
+  }
+  
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
+  build: { 
+    lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] },
+    rollupOptions: { 
+      external: [
+        'node:fs/promises'
+      ] 
+    },
+  },
   resolve: { alias: { src: resolve('src/') } },
+  plugins: [
+    nodePolyfills()
+  ],
 });
