a690755fd1ed7d1d7d68404e7af07b98d98a6a29
MattiaTaglia <mattia.tagliaferri9@gmail.com>
2024-07-09 17:44:52 +0200

    CBAM-29

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { builtinModules } from 'module';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,5 @@ export default defineConfig({
     },
   },
   resolve: { alias: { src: resolve('src/') } },
+  plugins: [dts()]
 });
