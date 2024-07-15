2528109e638192b63e22b51baa534ec3b82e713b
MattiaTaglia <mattia.tagliaferri9@gmail.com>
2024-07-15 09:59:31 +0200

    CBAM-29 - change in cjs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] },
+    lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['cjs'] },
     //target: 'node20',
     rollupOptions: {
       external: [...builtinModules, /^node:/],
