a83b058cfdafc17d1744784e11b681374e44f207
vincenzo di sieno <disienovincenzo@gmail.com>
2025-01-20 11:55:55 +0100

    CBAM-267

--- src/managers/prospetto.manager.ts
+++ src/managers/prospetto.manager.ts
@@ -1,8 +1,12 @@
-import { AdmDeclarationMapped, ProcessRequest, ProspettoSintesiManager } from "src/main";
-import { RichiestaProspettoSintesi } from "src/requests/ponImport/richiestaProspettoSintesiRequest";
-import { RichiestaProspettoSvincolo } from "src/requests/ponImport/richiestaProspettoSvincoloRequest";
-import ProspettoContabileManager from "./prospettoContabile.manager";
-import ProspettoSvincoloManager from "./prospettoSvincolo.manager";
+import {
+  AdmDeclarationMapped,
+  ProcessRequest,
+  ProspettoSintesiManager,
+} from 'src/main';
+import { RichiestaProspettoSintesi } from 'src/requests/ponImport/richiestaProspettoSintesiRequest';
+import { RichiestaProspettoSvincolo } from 'src/requests/ponImport/richiestaProspettoSvincoloRequest';
+import ProspettoContabileManager from './prospettoContabile.manager';
+import ProspettoSvincoloManager from './prospettoSvincolo.manager';
 
 export type ImportProspettoResult = {
   fileSvincolo: {
@@ -21,26 +25,29 @@ export type ImportProspettoResult = {
     extension: string;
   };
   admDeclarationMapped: AdmDeclarationMapped;
-}
+};
 
 export default class ProspettoManager {
   async import(
-    params: ProcessRequest<RichiestaProspettoSvincolo & RichiestaProspettoSintesi>,
+    params: ProcessRequest<
+      RichiestaProspettoSvincolo & RichiestaProspettoSintesi
+    >,
   ): Promise<ImportProspettoResult> {
     const sintesiManager = new ProspettoSintesiManager();
-    const { file: fileSintesi, admDeclarationMapped} = await sintesiManager.import(params)
+    const { file: fileSintesi, admDeclarationMapped } =
+      await sintesiManager.import(params);
 
     const contabileManager = new ProspettoContabileManager();
-    const { file: fileContabile } = await contabileManager.import(params)
+    const { file: fileContabile } = await contabileManager.import(params);
 
     const svincoloManager = new ProspettoSvincoloManager();
-    const { file: fileSvincolo } = await svincoloManager.import(params)
+    const { file: fileSvincolo } = await svincoloManager.import(params);
 
     return {
       fileContabile,
       fileSintesi,
       fileSvincolo,
-      admDeclarationMapped
-    }
+      admDeclarationMapped,
+    };
   }
 }
